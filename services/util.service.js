676bfc07c40adfe525be899e4ff40e1634e36a6e
Itaish-Bzu <Itaish94@gmail.com>
2025-08-04 17:09:41 +0300

    add function for make id

--- services/util.service.js
+++ services/util.service.js
@@ -7,3 +7,12 @@ export function readJsonFile(path) {
     const json = JSON.parse(str)
     return json
 }
+
+export function makeId(length = 5) {
+    let text = ''
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return text
+}
