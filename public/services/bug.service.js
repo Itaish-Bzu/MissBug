7cc38d68510593ed5e91b7412e724816b53cda75
Itaish-Bzu <Itaish94@gmail.com>
2025-08-04 18:52:37 +0300

    Add CRUDL and connect to axios

---
+++ public/services/bug.service.js
@@ -0,0 +1,56 @@
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const STORAGE_KEY = 'bugs'
+const BASE_URL = '/api/bug/'
+
+
+export const bugService = {
+  query,
+  getById,
+  save,
+  remove,
+  getDefaultFilter,
+}
+
+function query(filterBy) {
+  return axios
+    .get(BASE_URL)
+    .then((res) => res.data)
+    .then((bugs) => {
+      if (filterBy.txt) {
+        const regExp = new RegExp(filterBy.txt, 'i')
+        bugs = bugs.filter((bug) => regExp.test(bug.title))
+      }
+
+      if (filterBy.minSeverity) {
+        bugs = bugs.filter((bug) => bug.severity >= filterBy.minSeverity)
+      }
+
+      return bugs
+    })
+}
+
+function getById(bugId) {
+  return axios.get(BASE_URL + bugId).then((res) => res.data)
+}
+
+function remove(bugId) {
+  return axios.get(`${BASE_URL}${bugId}/remove`).then((res) => res.data)
+}
+
+function save(bug) {   
+  let url = BASE_URL + 'save'
+  let queryParams = `?title=${bug.title}&description=${bug.description}&severity=${bug.severity}&createdAt=${bug.createdAt}`
+  if (bug._id) queryParams += `&_id=${bug._id}`
+  return axios.get(url + queryParams)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log('err:', err)
+    })
+}
+
+
+function getDefaultFilter() {
+  return { txt: '', minSeverity: 0 }
+}
